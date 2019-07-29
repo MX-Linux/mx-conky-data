ad2d0e0809f54fcefed3dd823afca63584db8fd4
Jerry3904 <stormyjerry@gmail.com>
2019-07-28 20:15:23 -0400

    replaced existing with the MX-19 conkies

--- MX-Conkypatch/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mkdir -p ~/.conkypatch
-mkdir -p ~/.conkypatch/icons
-cp -v conky.conf ~/.conkypatch
-cp -v -r icons/* ~/.conkypatch/icons/
-mkdir -p ~/.fonts
-cp -v -r fonts ~/.fonts/
-
