9e5baf948609c7ced92cb5843c2a397e99577c6d
xrombik <xrombik@yandex.ru>
2020-03-27 23:45:30 +0300

    Fixes #16
    Now lsblk and awk are used to get a list of mounted partitions

--- MX-Asq/MX-GeekyTowerLogo/conky-harddisks.sh
+++ MX-Asq/MX-GeekyTowerLogo/conky-harddisks.sh
@@ -6,18 +6,7 @@
 # This file is released under the terms of the GNU GPL license, version 3 or later.
 #
 
-echo '   ${voffset -5}/:'
-echo '   ${voffset 4}${fs_used /} of ${fs_size /} ${alignr}${fs_bar 8,60 /}'
-echo
-
-# For now only for /home
-if [ "`mount | grep -w \"/home\" | awk '{ print $1 }'`" ]; then
-echo '   ${voffset -5}/home:'
-echo '   ${voffset 4}${fs_used /home} of ${fs_size /home} ${alignr}${fs_bar 8,60 /home}'
-echo
-fi
-
-LANG=C mount | grep "/media" | sed 's/.* on //g' | sed 's/type .*//g' | while read media; do
+LANG=C lsblk | grep 'part /' | awk -F' ' '{print $7}' | while read media; do
 echo '   ${voffset -5}'"$media":
 echo '   ${voffset 4}${fs_used' "$media"'} of ${fs_size' "$media"'} ${alignr}${fs_bar 8,60' "$media}"
 echo ''
